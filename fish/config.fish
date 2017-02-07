0fd095658548b6c6159fea8dae8f3997fd3b9f4d
Matthew Orndoff <mattheworndoff@gmail.com>
2017-02-06 22:38:57 -0800

    Remove unneeded comments from fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,3 @@
-# NOTE: Currently, fish is taking advantage of all Bash's env vars.
-# If you ever move to using fish w/o starting Bash
-# first, all custom env variable definitions must be moved here
-
-# Only differences are the declarations: 
-# export GREP_OPTIONS='--color=auto'
-# becomes
-# set -x GREP_OPTIONS '--color=auto'
-
 
 # ------ Aliases ----------
 function ef; vim ~/.config/fish/config.fish; end
