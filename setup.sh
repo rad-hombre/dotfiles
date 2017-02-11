a1aa569fb1bc5fd5a8daea1d559e0446df2fef55
Matthew Orndoff <mattheworndoff@gmail.com>
2017-02-10 20:23:33 -0800

    Make setup.sh a little more sain and readable
    Resolved symlink problems by deleting old dotfiles on intial run.
    Only clones in vundle if it's not already there.
    
    Script is now less verbose, used to spit out too much empty space
    into the terminal making everything hard to read – no more!

--- setup.sh
+++ setup.sh
@@ -4,32 +4,37 @@
 # Creates directory and dumps old dotfiles into it.
 # Next, creates symlinks in $HOME pointing to dotfiles in '~/dotfiles'
 
-dir=~/.dotfiles
-olddir=~/.dotfiles_old
+
+dotfiles=~/.dotfiles
+olddotfiles=~/.dotfiles_old
 
 files="aliases bashrc vimrc vim ssh tmux.conf inputrc bash_profile
 gitconfig"
 
-echo "Creating $olddir for backup of any existing dotfiles in \$HOME"
-mkdir -p $olddir
-echo "… mission accomplished!"
-echo ""
+if [ -d "$olddotfiles" ]; then
+    rm -rf $olddotfiles
+fi
+mkdir -p $olddotfiles
 
-cd $dir
+cd $dotfiles
+
+echo ""
+echo "Moving current dotfiles from \$HOME to $olddotfiles"
+
 for file in $files; do
-	echo "Moving current dotfiles from \$HOME to $olddir"
 	mv ~/.$file ~/.dotfiles_old/
-	echo ""
-	echo "Creating symlink to dotfiles/$file in \$HOME."
-	ln -s $dir/.$file ~/.$file
+	echo "Creating symlink to $dotfiles/.$file in \$HOME."
+	ln -s $dotfiles/.$file ~/.$file
 done 
 
-echo ""
-echo "Cloning Vundle,…"
-git clone https://github.com/gmarik/vundle ~/.vim/bundle/vundle
+if [ ! -d "$dotfiles/.vim/bundle/vundle" ]; then
+  echo ""
+  echo "Cloning Vundle,…"
+  git clone https://github.com/gmarik/vundle ~/.vim/bundle/vundle
+fi
 
 # Copy over fish 
-cp -r $dir/fish $dir/../.config/
+cp -r $dotfiles/fish $dotfiles/../.config/
 
 echo ""
 echo "That'll do, friendo… Enjoy!"
